d0a24e2d1910aae526df726c53476fcf11f6836d
SavokBS <88921908+SavokBS@users.noreply.github.com>
2022-01-29 14:52:07 +0500

    Update Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -9,10 +9,8 @@ settings = {
     "DumpPacket": False,
     "Blacklist": [24109],
     "DumpMajor": 41,
-    #"Tag": "Y92PLC2",
-    "Tag": "V8JV8Y98",
-    "ApiToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBlNTA5ZDM1LTk3M2ItNGFmMC05MDkwLTY4MTJiNWY2MTVlZiIsImlhdCI6MTY0MzIwODY0Mywic3ViIjoiZGV2ZWxvcGVyL2MyMzc5MGEyLWE2NjYtOTQ4Zi1mOWQ5LTY1NWFmYzY2NTY5YSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNzcuMjQyLjk3LjExIl0sInR5cGUiOiJjbGllbnQifV19.qVxM0eIw9FVNTvsmdFmYRIO4o63NZlsDyCNsdBRB8oeKePu6eho1E9qSmK9x-I4vXJOCHu0RdaglUwDdhv9JXg"
-    
+    "Tag": "Y92PLC2",
+    "ApiToken": "YOUR_TOKEN"
     
 
     #Put the tag ONLY FULL ACCOUNT, if there are less than 54 brawlers on the account, the server won't work!
